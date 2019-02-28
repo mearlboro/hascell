d30e3ac0f86a6622feccb002fccc857eb4272fa5
mearlboro <mearlboro@protonmail.com>
2019-02-28 22:42:28 +0000

    Code improvements - transform generation lists into array for faster indexing - add DerivingFunctor to W and removed instance definition - move helper functions inside show functions - better names for show functions

--- Simulate1D.hs
+++ Simulate1D.hs
@@ -1,20 +1,19 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Main where
 
 import Codec.Picture
 import Control.Comonad
+import Data.Array
 import Data.Bits
 import Data.Bits.Bitwise (fromListBE)
 import Data.Word
 
 --- Data Type
-data W a = W [a] a [a]
+data W a = W [a] a [a] deriving (Functor, Show)
 
 left, right :: W a -> W a
 left  (W (l:ls) x    rs ) = W    ls  l (x:rs)
 right (W    ls  x (r:rs)) = W (x:ls) r rs
-
-instance Functor W where
-    fmap f (W as x bs) = W (fmap f as) (f x) (fmap f bs)
 
 instance Comonad W where
     extract (W _ x _) = x
@@ -42,32 +41,28 @@ wolframRule x w = testBit x $ fromListBE (fmap extract [left w, w, right w])
 
 
 --- Print to terminal
-showCell :: Bool -> String
-showCell True  = "██"
-showCell False = "  "
-
-printRun :: Word8 -> W Bool -> Int -> Int -> IO ()
-printRun r w n d = mapM_ putStrLn generations
+stringShow :: Word8 -> W Bool -> Int -> Int -> IO ()
+stringShow r w n d = mapM_ putStrLn generations
     where
         generations = map showGen $ run (wolframRule r) w n d
         showGen cs = concat $ map showCell cs
+        showCell True  = "██"
+        showCell False = "  "
 
-
---- Print to image
-showPixel :: Bool -> PixelRGB8
-showPixel True  = PixelRGB8 0x00 0x00 0x00
-showPixel False = PixelRGB8 0xff 0xff 0xff
-
-imgRun :: Word8 -> W Bool -> Int -> Int -> DynamicImage
-imgRun r w n d = ImageRGB8 $ generateImage getPixel (d * 2+1) n
+--- Generate image
+imgShow :: Word8 -> W Bool -> Int -> Int -> DynamicImage
+imgShow r w n d = ImageRGB8 $ generateImage getPixel (d * 2 + 1) n
     where
-        getPixel x y = showPixel $ generations !! y !! x
-        generations  = run (wolframRule r) w n d
+        getPixel x y = showPixel $ image ! y ! x
+        image = fmap (listArray (0, d * 2)) $ listArray (0, n - 1) generations
+        generations = run (wolframRule r) w n d
+        showPixel True  = PixelRGB8 0x00 0x00 0x00
+        showPixel False = PixelRGB8 0xff 0xff 0xff
 
 main :: IO()
 main = sequence_ $ map save [0..255]
     where
-        save r = savePngImage (imgPath r) (imgRun r wolframWorld n d)
+        save r = savePngImage (imgPath r) (imgShow r wolframWorld n d)
         imgPath r = "img/rule" ++ show r ++ ".png"
         rs = [0..255]
         n  = 1024
