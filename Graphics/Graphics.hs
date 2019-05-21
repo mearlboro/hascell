0c0769cdc74b4cc257fcac4aa1e55ab53340f784
M <mearlboro@protonmail.com>
2019-05-21 21:27:18 +0100

    Add scale to image generation

--- Graphics/Graphics.hs
+++ Graphics/Graphics.hs
@@ -11,25 +11,25 @@ module Hascell.Graphics where
 
     type ColourMap a = (a -> PixelRGB8)
 
-    pngFrom1D :: ColourMap a -> [U Int a] -> DynamicImage
-    pngFrom1D colours us = ImageRGB8 $ generateImage getPixel w h
+    pngFrom1D :: ColourMap a -> Int -> [U Int a] -> DynamicImage
+    pngFrom1D colours zoom us = ImageRGB8 $ generateImage getPixel w h
         where
-            getPixel x y = colours $ pixelArray ! y ! x
+            getPixel x y = colours $ pixelArray ! (y `div` zoom) ! (x `div` zoom)
             pixelArray = listArray (0, h) $ map arr us
-            w = length $ arr $ head us
-            h = length us
+            w = zoom * (length $ arr $ head us)
+            h = zoom * length us
 
-    pngFrom2D :: ColourMap a -> U (Int, Int) a -> DynamicImage
-    pngFrom2D colours u = ImageRGB8 $ generateImage getPixel w h
+    pngFrom2D :: ColourMap a -> Int -> U (Int, Int) a -> DynamicImage
+    pngFrom2D colours zoom u = ImageRGB8 $ generateImage getPixel w h
         where
-            getPixel x y = colours $ (arr u) ! (x, y)
-            h = height u
-            w = width  u
+            getPixel x y = colours $ (arr u) ! (x `div` zoom, y `div` zoom)
+            h = zoom * height u
+            w = zoom * width  u
 
-    exportGifFrom2D :: [U (Int, Int) a] -> ColourMap a -> Int -> String -> Either String (IO ())
-    exportGifFrom2D us colours delay path = writeGifAnimation path delay LoopingForever imgs
+    exportGifFrom2D :: [U (Int, Int) a] -> ColourMap a -> Int -> Int -> String -> Either String (IO ())
+    exportGifFrom2D us colours zoom delay path = writeGifAnimation path delay LoopingForever imgs
         where
-            imgs = map (dynToImg . pngFrom2D colours) us
+            imgs = map (dynToImg . pngFrom2D colours zoom) us
             dynToImg (ImageRGB8 img) = img
 
 --- Export Wolfram
@@ -40,13 +40,13 @@ module Hascell.Graphics where
     wolframPath :: Word8 -> String
     wolframPath r = "img/wolfram/rule_" ++ show r ++ ".png"
 
-    wolframExport :: Int -> Int -> Word8 -> IO ()
-    wolframExport n d r = savePngImage (wolframPath r) img
+    wolframExport :: Int -> Int -> Int -> Word8 -> IO ()
+    wolframExport n d zoom r = savePngImage (wolframPath r) img
         where
-            img = pngFrom1D wolframPixels (run (wolframRule r) (wolframWorld d) n)
+            img = pngFrom1D wolframPixels zoom (run (wolframRule r) (wolframWorld d) n)
 
-    wolframExportAll :: Int -> Int -> IO ()
-    wolframExportAll n d = sequence_ $ map (wolframExport n d) [0..255]
+    wolframExportAll :: Int -> Int -> Int -> IO ()
+    wolframExportAll n d zoom = sequence_ $ map (wolframExport n d zoom) [0..255]
 
 --- Export Conway
     conwayPixels :: ColourMap Bool
@@ -56,10 +56,10 @@ module Hascell.Graphics where
     conwayPath :: Show a => a -> String -> String
     conwayPath w ext = "img/conway/pattern_" ++ show w ++ "." ++ ext
 
-    conwayExportStep :: GameOfLife -> Int -> IO ()
-    conwayExportStep w n = savePngImage (conwayPath n "png") img
+    conwayExportStep :: GameOfLife -> Int -> Int -> IO ()
+    conwayExportStep w n zoom = savePngImage (conwayPath n "png") img
         where
-            img = pngFrom2D conwayPixels (run gameOfLife w n !! (n - 1))
+            img = pngFrom2D conwayPixels zoom (run gameOfLife w n !! (n - 1))
 
-    conwayExport :: GameOfLife -> Int -> IO ()
-    conwayExport w n = either print id $ exportGifFrom2D (run gameOfLife w n) conwayPixels 10 (conwayPath n "gif")
+    conwayExport :: GameOfLife -> Int -> Int -> IO ()
+    conwayExport w n zoom delay = either print id $ exportGifFrom2D (run gameOfLife w n) conwayPixels zoom delay (conwayPath n "gif")
