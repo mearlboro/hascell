72746967b5645692482e4d73feebc9cec979eb87
M <mearlboro@protonmail.com>
2020-04-28 19:16:58 +0100

    Add a filename parameter to functions generating Game of Life files

--- src/Hascell/Graphics.hs
+++ src/Hascell/Graphics.hs
@@ -53,13 +53,13 @@ module Hascell.Graphics where
     conwayPixels True  = PixelRGB8 0x00 0x00 0x00
     conwayPixels False = PixelRGB8 0xff 0xff 0xff
 
-    conwayPath :: Show a => a -> String -> String
-    conwayPath u ext = "img/conway/pattern_" ++ show u ++ "." ++ ext
+    conwayPath :: String -> String -> String
+    conwayPath name ext = "img/conway/pattern_" ++ name ++ "." ++ ext
 
-    conwayExportStep :: GameOfLife -> Int -> Int -> IO ()
-    conwayExportStep u n zoom = savePngImage (conwayPath u "png") img
+    conwayExportStep :: String -> GameOfLife -> Int -> Int -> IO ()
+    conwayExportStep name u n zoom = savePngImage (conwayPath name "png") img
         where
             img = pngFrom2D conwayPixels zoom (run gameOfLifeRule u n !! (n - 1))
 
-    conwayExport :: GameOfLife -> Int -> Int -> Int -> IO ()
-    conwayExport u n zoom delay = either print id $ exportGifFrom2D (run gameOfLifeRule u n) conwayPixels zoom delay (conwayPath u "gif")
+    conwayExport :: String -> GameOfLife -> Int -> Int -> Int -> IO ()
+    conwayExport name u n zoom delay = either print id $ exportGifFrom2D (run gameOfLifeRule u n) conwayPixels zoom delay (conwayPath name "gif")
