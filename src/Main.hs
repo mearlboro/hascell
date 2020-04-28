a0dc939c068095d15cf951638e03ee036e3f6311
M <mearlboro@protonmail.com>
2020-04-28 19:26:38 +0100

    Add some simple code to main which reads a file and creates a GoL GIF

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
+import Hascell.Conway
 import Hascell.Graphics
+import System.IO  
+import System.FilePath
+      
+parseInput :: String -> [(Int, Int)]
+parseInput x = map pairs . map (map read) . map words . lines $ x
+    where
+        pairs (x:y:_) = (x,y)
+
+stringRunGameOfLife :: IO ()
+stringRunGameOfLife = do
+    pattern <- getLine
+    contents <- readFile ("meta/patterns/" ++ pattern) 
+    u <- return . patternGameOfLife . parseInput $ contents
+    stringShow u
+
+imgRunGameOfLife :: IO ()
+imgRunGameOfLife = do
+    pattern <- getLine
+    contents <- readFile ("meta/patterns/" ++ pattern) 
+    u <- return . patternGameOfLife . parseInput $ contents
+    conwayExport pattern u 100 5 10
+
 
 main :: IO ()
-main = wolframExport 1024 1024 5 30
+main = putStrLn "Welcome to Hascell"
