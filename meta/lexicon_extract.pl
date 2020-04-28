d89a0cd77ab3101c01db1c9bf4c0fde228798f52
M <mearlboro@protonmail.com>
2020-04-28 15:41:17 +0100

    Add 298 known GoL patterns as files in meta/patterns
    - the files have been generated with the Perl script
    - each file contains the x-y coordinates of an alive cell

--- meta/lexicon_extract.pl
+++ meta/lexicon_extract.pl
@@ -12,50 +12,50 @@ map { print_coordinates($_, $lexicon->{$_}) } (keys %$lexicon);
 # Extracts the names and patterns (expressed as strings) from the HTML
 # Returns a hash with the pattern name as keys and pattern strings as values
 sub get_all_pattern_string {
-	my ($file) = @_;
+    my ($file) = @_;
 
-	open(my $fh, '<', $file);
-	my ($name, $pattern, %lexicon);
+    open(my $fh, '<', $file);
+    my ($name, $pattern, %lexicon);
 
-	while(my $line = <$fh>) {
-		chomp $line;
-		if ($line =~ /:<b>([^<]+)<\/b>/i) {
-			$name = $1;
+    while(my $line = <$fh>) {
+        chomp $line;
+        if ($line =~ /:<b>([^<]+)<\/b>/i) {
+            $name = $1;
 
-			# Modify name to remove spaces, punctuation and make camel case
-			$name =~ s/[\s'-\/\$](\w)/\U$1\L/g;
-			$name =~ s/\+/Plus/g;
-			$name =~ s/^([a-z])/\U$1\L/g;
-		}
-		elsif ($line =~ /<pre>/i) {
-			$pattern = "";
-			$line = <$fh>;
-			while ($line =~ /([.O]+)\s*/i) {
-				$pattern .= "$1\n";
-				$line = <$fh>;
-				if ($line =~ /<\/pre>/) {
-					$lexicon{$name} = $pattern;
-					last;
-				}
-			}
-		}
-	}
-	close $fh;
+            # Modify name to remove spaces, punctuation and make camel case
+            $name =~ s/[\s'-\/\$](\w)/\U$1\L/g;
+            $name =~ s/\+/Plus/g;
+            $name =~ s/^([a-z])/\U$1\L/g;
+        }
+        elsif ($line =~ /<pre>/i) {
+            $pattern = "";
+            $line = <$fh>;
+            while ($line =~ /([.O]+)\s*/i) {
+                $pattern .= "$1\n";
+                $line = <$fh>;
+                if ($line =~ /<\/pre>/) {
+                    $lexicon{$name} = $pattern;
+                    last;
+                }
+            }
+        }
+    }
+    close $fh;
 
-	return \%lexicon;
+    return \%lexicon;
 }
 
 
 # Generates a file with the row and column coordinates of each alive cell
 sub print_coordinates {
-	my ($name, $pattern) = @_;
+    my ($name, $pattern) = @_;
 
     open my $out, '>>', "patterns/$name";
 
-	my @sparse = pattern_string_to_sparse_matrix($pattern);
+    my @sparse = pattern_string_to_sparse_matrix($pattern);
 
     foreach (pairs(@sparse)) {
-		my ($x, $y) = @$_;
+        my ($x, $y) = @$_;
         print $out "$x $y\n";
     }
 
@@ -75,20 +75,20 @@ sub print_coordinates {
 #                                      (2, 1)       
        
 sub pattern_string_to_sparse_matrix {
-	my ($pattern) = @_;
+    my ($pattern) = @_;
 
-	chomp $pattern;
-	my @sparse = ();
+    chomp $pattern;
+    my @sparse = ();
 
-	my @lines = split('\n',$pattern);
-	for (my $i = 0; $i < scalar(@lines); $i++) {
-		my @chars = split('', $lines[$i]);
-		for (my $j = 0; $j < scalar(@chars); $j++) {
-			if ($chars[$j] eq 'O') {
-				push @sparse, ($i, $j);
-			}
-		}
-	}
+    my @lines = split('\n',$pattern);
+    for (my $i = 0; $i < scalar(@lines); $i++) {
+        my @chars = split('', $lines[$i]);
+        for (my $j = 0; $j < scalar(@chars); $j++) {
+            if ($chars[$j] eq 'O') {
+                push @sparse, ($i, $j);
+            }
+        }
+    }
 
-	return @sparse;
+    return @sparse;
 }
